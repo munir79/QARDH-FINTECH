23fe43bd10b250e2b8e7f1bfea63425a4cd99c19
munir79 <jakirhossainmunir79@gmail.com>
2025-07-25 17:07:33 +0600

    redux setup

---
+++ src/redux/apiSlice/apiSlice.js
@@ -0,0 +1,15 @@
+"use client";
+
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: '/',
+    // credentials:'include',
+  }),
+
+  tagTypes: ["users"],
+
+  endpoints: () => ({}),
+});
